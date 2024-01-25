626f904e0b0f3b6bd46e4fd9b8610df11cdde810
TI Sabbir <tisabbir0@gmail.com>
2024-01-25 23:03:55 +0600

    some variables are added

--- Intro to js/first.js
+++ Intro to js/first.js
@@ -1,2 +1,16 @@
 var myNewNumber = 134;
 console.log(myNewNumber);
+
+console.log(typeof myNewNumber);
+
+myNewNumber = true;
+console.log(typeof myNewNumber);
+myNewNumber = 'Lol'
+console.log(typeof myNewNumber);
+myNewNumber = NaN;
+console.log(typeof myNewNumber)
+myNewNumber = [1,2,3]
+console.log(typeof myNewNumber);
+myNewNumber = { name: 'sabik'}
+console.log(typeof myNewNumber);
+console.log(typeof myNewNumber);
