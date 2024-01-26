b928a2e1bcab54d5b184f135120babc5d5d9fd21
TI Sabbir <tisabbir0@gmail.com>
2024-01-26 08:56:18 +0600

    task2 finished Alhamdulillah

---
+++ Intro to js/task2.js
@@ -0,0 +1,14 @@
+var Mathematics = 89, Biology = 81, Chemistry=96, Physics = 78, Bangla =85;
+
+var totalMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+
+var avgMarks = totalMarks/5;
+
+console.log(avgMarks)
+
+75.25, 65, 80, 35.45, 99.50
+var Mathematics = 75.25, Biology = 65, Chemistry=80, Physics = 35.45, Bangla =99.50;
+
+var totalMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+var avgMarks = totalMarks/5;
+console.log(avgMarks.toFixed(2))
