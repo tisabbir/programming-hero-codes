c5702dea4a9b3256ee6ea49cea7bb6f4b59dc9a7
TI Sabbir <tisabbir0@gmail.com>
2024-01-27 10:44:40 +0600

    the if-else conditional is added

---
+++ Milestone-4/Fundamental-Conditionals/if-else.js
@@ -0,0 +1,14 @@
+if (50>10) { 
+    console.log('I have a smaller number')
+}
+else {
+    console.log('I have larger number')
+}
+
+let weight = 50;
+if(weight<=20) {
+    console.log("I will bear on foot")
+} 
+else {
+    console.log("I will rent a rickshaw")
+}
