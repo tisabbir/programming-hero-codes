31b23028b35da578a0da4d58f3a90f33e465dde3
TI Sabbir <tisabbir0@gmail.com>
2024-01-27 10:09:33 +0600

    comparison js added

---
+++ Milestone-4/Fundamental-Conditionals/comparision.js
@@ -0,0 +1,8 @@
+console.log(5>19);
+console.log(5<19);
+console.log(5==19);
+console.log(5<=19);
+console.log(5>=19);
+console.log(5== '5');
+console.log(5=== '5');
+console.log(5<19);
---
+++ Milestone-4/Fundamental-Conditionals/variable.js
@@ -0,0 +1,3 @@
+var sunglassPrice = 80; //Number
+var name = 'Sabbir'; //String
+var isPassed = true; //Boolean
