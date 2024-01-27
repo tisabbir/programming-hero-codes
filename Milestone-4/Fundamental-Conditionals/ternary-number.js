d9153519fbf7e10ea17230158c3b853ed54b6930
TI Sabbir <tisabbir0@gmail.com>
2024-01-27 21:33:21 +0600

    ternary number solved

---
+++ Milestone-4/Fundamental-Conditionals/ternary-number.js
@@ -0,0 +1,18 @@
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+let num1 = 16;
+let num2 = 15;
+let result;
+result = num1>num2 ? num1 * 2 : num1 + num2;
+
+console.log(result)
