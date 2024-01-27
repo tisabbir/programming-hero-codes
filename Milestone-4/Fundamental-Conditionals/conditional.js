565deaa908a93cb3a439e15307e91745c780aff2
TI Sabbir <tisabbir0@gmail.com>
2024-01-27 13:11:15 +0600

    condiional js added

---
+++ Milestone-4/Fundamental-Conditionals/conditional.js
@@ -0,0 +1,20 @@
+let height = 65;
+let hasCar = true;
+
+let isBCS = true;
+let hasBariInBosundhara = true;
+
+// if(height > 68 || hasCar == true) {
+//     console.log('Onek SU---partro to tumi')
+// } else {
+//     console.log("Dure gia mor")
+// }
+
+
+//Complex Conditionals
+
+if((height>68 && isBCS ==true) || (hasBariInBosundhara ==true && hasCar ==true)) {
+    console.log('Onek SU---partro to tumi')
+} else {
+    console.log("Dure gia mor")
+}
