f7e9697343ac1d5056198a012a7c2a60bfb9f24e
TI Sabbir <tisabbir0@gmail.com>
2024-01-27 14:20:12 +0600

    restaurant discount based on age is added

---
+++ Milestone-4/Fundamental-Conditionals/restaurant.js
@@ -0,0 +1,14 @@
+let age = 5;
+
+if ( age <= 12) {
+    console.log("You can eat for free. No money needed")
+}
+else if(age >=60 ){
+    console.log("Murubbi You are discounted 50% Tk")
+}
+else if (age >=50) {
+    console.log("You will get 25% discount")
+}
+else {
+    console.log("You have to pay full money")
+}
