18acf4889ff018f38f74bfe210b75e513d25866b
TI Sabbir <tisabbir0@gmail.com>
2024-01-27 20:05:58 +0600

    nested conditionals added

---
+++ Milestone-4/Fundamental-Conditionals/nested-conditions.js
@@ -0,0 +1,33 @@
+let money = -100;
+
+if(money>1000) {
+    console.log('Bro you are rich, You are my friend')
+}
+else {
+    if(money>800){
+        console.log('Mamma Tui to heavy borolok hoy jaccis')
+    }
+    else {
+        if(money > 500 ) {
+            console.log('Vhai buke aai, amra moddo bitto')
+        }
+        else {
+            if(money>200) {
+                console.log("Bondu Hard work kor")
+            }
+            else{
+                if(money>100) {
+                    console.log("Amar 7e kom misbi")
+                }
+                else {
+                    if(money>0) {
+                        console.log("I don't know you")
+                    }
+                    else {
+                        console.log("Dhor 20 taka ne, cha khais.")
+                    }
+                }
+            }
+        }
+    }
+}
