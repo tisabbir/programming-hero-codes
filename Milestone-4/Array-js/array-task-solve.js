2aee7cbbcc3bfac230b060b782c00c396b78fd16
TI Sabbir <tisabbir0@gmail.com>
2024-01-29 20:12:01 +0600

    task 3 solved

--- Milestone-4/Array-js/array-task-solve.js
+++ Milestone-4/Array-js/array-task-solve.js
@@ -1,6 +1,22 @@
-const arr = ['aam', 'jam', 'kola', 'kathal', 'lichu'];
+// const arr = ['aam', 'jam', 'kola', 'kathal', 'lichu'];
 
-console.log(arr[2]);
+// console.log(arr[2]);
 
-arr[1] ='jambura'
-console.log(arr)
+// arr[1] ='jambura'
+// console.log(arr)
+
+
+//Task 2
+//2
+
+// const touristSpots = ['bandarban', 'sundarban', 'Kandarban'];
+// console.log(touristSpots);
+// touristSpots.push('Tandarban');
+// console.log(touristSpots);
+// touristSpots.push('Landarban','Tendarban');
+// console.log(touristSpots);
+// touristSpots.pop();
+// console.log(touristSpots);
+
+
+// Task 3
