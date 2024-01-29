29c22d809e8d0b4f763f9c1e10d0935421ba88fb
TI Sabbir <tisabbir0@gmail.com>
2024-01-29 13:54:41 +0600

    js array task 1st  solutions  added

---
+++ Milestone-4/Array-js/array-task-solve.js
@@ -0,0 +1,6 @@
+const arr = ['aam', 'jam', 'kola', 'kathal', 'lichu'];
+
+console.log(arr[2]);
+
+arr[1] ='jambura'
+console.log(arr)
---
+++ Milestone-4/Array-js/js-array-task.js
@@ -0,0 +1,44 @@
+/*
+js-array-tasks
+
+
+1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+
+
+
+2. Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output
+3. Checking Array Membership with ‘includes’
+Instructions:
+
+Create an array of books containing different book.
+
+Use the includes method to check if the array contains a javascript book.
+
+Print a message to the console indicating whether the element is present in the array or not.
+
+4. Checking if it's an Array
+Instructions:
+
+Create different variables, each containing either an array or a non-array value.
+
+Now use isArray to check if each variable is an array.
+
+Print a message to the console indicating whether each variable is an array or not.
+
+5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+
+*/
