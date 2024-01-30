b5beb2f8d08773571965c860cc76f6a04fb9ab28
TI Sabbir <tisabbir0@gmail.com>
2024-01-31 04:27:31 +0600

    string-object added

---
+++ Milestone-4/String-Object/string.js
@@ -0,0 +1,8 @@
+const nam = 'Sabbir';
+const nam2 = "Sabbir";
+const nam3 = `Sabbir`;
+const nam4 = new String('Sabbir');
+// console.log(nam , nam2, nam3, nam4)
+
+// String is one kind of array but immutable
+console.log(nam.length)
