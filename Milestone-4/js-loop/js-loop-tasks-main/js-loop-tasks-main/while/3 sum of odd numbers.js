6e757668339888e59f937d5a1d2141e5e12601e7
TI Sabbir <tisabbir0@gmail.com>
2024-01-30 14:33:20 +0600

    PROBLEM 3 solved

--- Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/while/3 sum of odd numbers.js
+++ Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/while/3 sum of odd numbers.js
@@ -6,7 +6,23 @@ Subtask-1:
 
 Display sum of all the odd numbers from 81 to 131.
 
+
+
  */
+
+let num = 81;
+let sum = 0;
+
+while(num<=131){
+    
+    if(num%2!==0){
+        sum = sum + num;
+        
+    }
+    num++;
+}
+console.log(sum);
+
 /***
 
 Subtask-2:
@@ -14,5 +30,16 @@ Subtask-2:
 Display sum of all the even numbers from 206 to 311.
 
  */
+
+let num2 = 206;
+let sum2 = 0;
+
+while(num2<=311){
+    if(num2%2===0){
+        sum2 = sum2 + num2;
+    }
+    num2++;
+}
+console.log(sum2)
 
 /*programming hero*/
