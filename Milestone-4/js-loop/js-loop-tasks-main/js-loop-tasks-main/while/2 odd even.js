4a2ef761bf4f6a6d40eb9332a6d0d5cccabfde3b
TI Sabbir <tisabbir0@gmail.com>
2024-01-30 11:11:48 +0600

    problem 2 of while loop is solved

--- Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/while/2 odd even.js
+++ Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/while/2 odd even.js
@@ -6,6 +6,18 @@ Find all the odd numbers from 61 to 100.
 
  */
 
+// let numbers = 61;
+
+// while(numbers <=100) {
+//     if(numbers%2===0){
+//         //do noting
+//     } else {
+//         console.log(numbers)
+//     }
+//     numbers++;
+// }
+
+
 
 /***
 
@@ -14,5 +26,15 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
+
+let num = 78;
+
+while(num < 100) {
+
+    if(num%2===0){
+        console.log(num)
+    }
+    num++;
+}
 
 /*programming hero*/
