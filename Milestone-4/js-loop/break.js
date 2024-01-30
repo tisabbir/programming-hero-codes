f227fdb7286faecdad0826db03e386ce6ed41324
TI Sabbir <tisabbir0@gmail.com>
2024-01-30 18:59:32 +0600

    break adde

---
+++ Milestone-4/js-loop/break.js
@@ -0,0 +1,7 @@
+for (let i = 1; i<=15; i++){
+    console.log(i)
+    if(i>=5){
+        break;
+    }
+}
+console.log('khela sesh')
---
+++ Milestone-4/js-loop/different-ways.js
@@ -0,0 +1,6 @@
+// Give the numbers which are divided by 3 or/and 5 between 1 to 30
+for(let i=1; i<=30; i++){
+    if(i%3===0 || i%5===0){
+        console.log(i)
+    }
+}
