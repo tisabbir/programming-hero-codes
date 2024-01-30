ec47da5238e408cb6f5ca636a2acaf19dc1081ed
TI Sabbir <tisabbir0@gmail.com>
2024-01-30 07:53:08 +0600

    loop of js files are added

---
+++ Milestone-4/js-loop/for-of.js
@@ -0,0 +1,6 @@
+const numbers = [1, 3, 5, 23, 5, 345, 345, 77];
+console.log(numbers);
+
+for(const num of numbers) {
+    console.log(num)
+}
---
+++ Milestone-4/js-loop/while.js
@@ -0,0 +1,6 @@
+let num = 0;
+
+while(num<5) {
+    console.log('looping ' + num);
+    num++;
+}
