4b01c57fff070fb796f82078fad433d7ae5eda2d
TI Sabbir <tisabbir0@gmail.com>
2024-01-30 11:05:21 +0600

    commitment problem solved

---
+++ Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/while/1 commitment.js
@@ -0,0 +1,14 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+let repeat = 1;
+
+while (repeat <=60) {
+    console.log("I will invest at least 6 hrs every single day for next 60 days!", repeat)
+    repeat++;
+}
