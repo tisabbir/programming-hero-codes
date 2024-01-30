1dd96430a09179ef2fd07098bac473fd58ae9778
TI Sabbir <tisabbir0@gmail.com>
2024-01-31 04:32:55 +0600

    string methods are added

--- Milestone-4/String-Object/string.js
+++ Milestone-4/String-Object/string.js
@@ -1,8 +1,25 @@
 const nam = 'Sabbir';
-const nam2 = "Sabbir";
-const nam3 = `Sabbir`;
+const nam2 = "sabbir";
+const nam3 = `Sabbir    `;
 const nam4 = new String('Sabbir');
+const nam5 = `    Sabbir`;
 // console.log(nam , nam2, nam3, nam4)
 
 // String is one kind of array but immutable
-console.log(nam.length)
+// console.log(nam[2])
+
+//toUpperCase
+
+console.log(nam.toUpperCase());
+console.log(nam.toLowerCase());
+
+//Compare 2 string
+
+console.log(nam == nam2);
+console.log(nam.toLowerCase() == nam2.toLowerCase());
+
+console.log(nam3==nam5);
+console.log(nam3.trim()==nam5.trim());
+
+
+
