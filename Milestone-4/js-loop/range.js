a8312523ee3ad1f5db73ca8dd7eec45e208bf423
TI Sabbir <tisabbir0@gmail.com>
2024-01-30 08:13:10 +0600

    even number checker loop js files are added

--- Milestone-4/js-loop/range.js
+++ Milestone-4/js-loop/range.js
@@ -1,6 +1,11 @@
 let num = 1;
-
+let sum = 0;
 while(num <= 10) {
-    console.log(num);
+    console.log('The number is ' + num);
+    
+    sum = sum + num;
     num++;
+    console.log( 'The sum is ' + sum);
 }
+
+console.log('The total sum of the numbers 1 to 10 is: ' + sum);
