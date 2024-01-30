81ff3af6006fa280ec9c5f3652ade6963c3f6997
TI Sabbir <tisabbir0@gmail.com>
2024-01-30 16:12:44 +0600

    problem 1 of for loop solved

--- Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/for/1 commitment.js
+++ Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/for/1 commitment.js
@@ -4,4 +4,9 @@
 
  */
 
+
+for(let i = 1; i<=60; i++){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!" , i)
+}
+
 /*programming hero*/
--- Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/while/5 countdown.js
+++ Milestone-4/js-loop/js-loop-tasks-main/js-loop-tasks-main/while/5 countdown.js
@@ -4,5 +4,12 @@ Implement a countdown timer that counts down from 21 to 15.
 
  */
 
+let count = 21;
+
+while(count >=15){
+
+    console.log(count)
+    count--;
+}
 
 /*programming hero*/
