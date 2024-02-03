40a440d19cba91147f6a4868637f155c56a70ec5
TI Sabbir <tisabbir0@gmail.com>
2024-02-03 15:16:07 +0600

    fundamental of function is added

---
+++ Milestone-4/Fundamental-functions/function.js
@@ -0,0 +1,18 @@
+let gumAse = false;
+let bathRoomChapse = true;
+
+function porteBosho (){
+    if(gumAse === true){
+        console.log('Coffee Khaw')
+    }
+    if(bathRoomChapse === true){
+        console.log('Bath Room e jaw')
+    }
+    console.log('Ozu koro')
+    console.log('Boi Khata kolom laptop ready koro')
+    console.log('Dowa koro')
+    console.log('Target Set koro')
+    console.log('Pora Shuru koro')
+    
+}
+porteBosho();
