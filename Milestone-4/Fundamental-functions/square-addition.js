3b9aa00fac8a5b560b42bc51d8ed02c76c07106e
TI Sabbir <tisabbir0@gmail.com>
2024-02-04 09:49:08 +0600

    square addition function added

---
+++ Milestone-4/Fundamental-functions/square-addition.js
@@ -0,0 +1,15 @@
+function square (num){
+    let sq = num * num;
+    console.log('Square of the number is : ', sq)
+}
+
+square(6)
+
+
+// Function to add three numbers 
+
+function addidtion ( a ,b ,c){
+    let add = a+b+c;
+    console.log('Addition of the provided three numbers is : ', add)
+}
+addidtion(4,5,6)
