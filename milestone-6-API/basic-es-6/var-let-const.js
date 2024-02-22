c285651778e2763681dc353f6eef47b5918f2703
TI Sabbir <tisabbir0@gmail.com>
2024-02-22 06:50:00 +0600

    var let const diff added

---
+++ milestone-6-API/basic-es-6/var-let-const.js
@@ -0,0 +1,11 @@
+/**
+ * Var --> We should never use it 
+ * let --> when the variable needs to be changed (10% cases)
+ * const --> when the variable don't needs to be changed (90% cases)
+ * */ 
+
+/**
+ * The array and the objects can be modified when declared as const 
+ * But they cannot be reassigned when used as an array
+ * 
+ */
