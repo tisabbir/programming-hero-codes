5a002f3591c5691bfdad496d783d762c1faf3af0
TI Sabbir <tisabbir0@gmail.com>
2024-02-24 13:09:47 +0600

    practice done alhamdulillah

---
+++ milestone-6-API/advanced-es-6/dotvsbracket.js
@@ -0,0 +1,11 @@
+const person = {
+    name : 'kadam ali',
+    job : 'khai badam khali',
+    7 : 'age tar',
+    'se-kore' : 'ulta palta'
+}
+
+console.log(person.name);
+console.log(person.job);
+console.log(person['7']);
+console.log(person["se-kore"])
