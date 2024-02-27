6db89bd1a3955e14fd65e82f6ddd580f381a74bf
TI Sabbir <tisabbir0@gmail.com>
2024-02-27 07:44:36 +0600

    json file object created

---
+++ milestone-6-API/API intro/json.js
@@ -0,0 +1,14 @@
+const shop = {
+    name : 'eStall',
+    owner : ['TI Sabbir', 'Mahdi', 'Hasib'],
+    shopServices : {
+        platform : ['iOs', 'android', 'windows'],
+        howOperate : function operation(order){
+            console.log('send it to our local agents')
+        }
+    },
+    isProfitable : true,
+    monthlyRevenue : 4500000000,
+    isOpen : false,
+
+}
